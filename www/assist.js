af95dec4a1bf3218c33e2fa7b7550c0d3fc44a43
miracle0k <kmiracle@gmail.com>
2009-11-14 06:13:41 +0000

    ^^
    git-svn-id: http://svn.pentome.com/wordassist/trunk@15 f7c9c6a8-60cf-4ce6-9e6a-3af6cb210574

--- www/assist.js
+++ www/assist.js
@@ -5,16 +5,14 @@ var Assist = function(options) {
 		return;
 	}
 	
-	var callback = options.callback || function() {alert("Options havn't callback function");};
-    
     var self = this;
-    
+	var callback = options.callback || function() {alert("Options havn't callback function");};
     var element = jQuery('#assist');
     
     //한글채크            
     var isHangul = function(str) {
-        han = /[ㄱ-힣]/g;
-        chk_han = str.match(han);
+        var han = /[ㄱ-힣]/g;
+        var chk_han = str.match(han);
 
         if(str.length === chk_han.length) {
             return true;
@@ -25,23 +23,40 @@ var Assist = function(options) {
 
     //영어 채크
     var isEnglish = function(str) {
-        eng = /[a-z|A-Z]/g;
-        chk_eng = str.match(eng);
+        var eng = /[a-z|A-Z]/g;
+        var chk_eng = str.match(eng);
 
         if(str.length === chk_eng.length) {
             return true;
         } else {
             return false;
         }        
-    };
+    };    
+    
+    var insertItems = function(list) {
+        console.log(list);
+        var i=0;
+        for(i=0 ; i < list.length; i++) {
+        	console.log(list[i]);
+        	element.appendTo("<li>"+ list[i].word +" : " + list[i].desc +"</li>");
+        }
+    }
 
     var korean = function(q) {
         var API_KEY = "7346eb8352778b08bb5bedb006f1bee3bd4375e8";
         var URL = "http://apis.daum.net/dic/krdic";
         
         jQuery.getJSON(URL + "?apikey="+ API_KEY + "&q=" + q + "&kind=WORD&callback=?&output=json",
-            function(data) {                 
-                console.log(data);
+            function(data) {
+            	var i =0;
+                var channel = data.channel;
+                
+                var list = [];                
+                for(i=0; i<channel.result; i++) {
+                    list.push({word : channel.item[i].title, desc : channel.item[i].description});	
+                }
+                
+                insertItems(list);
         });
     };
 
@@ -57,7 +72,7 @@ var Assist = function(options) {
         }
         
         if(isHangul(str)) {
-            
+            korean(str);
         } else if(isEnglish(str)) {
             
         } else {
@@ -72,6 +87,8 @@ var Assist = function(options) {
     };
     
     this.close = function(isCallCallBackFunc) {
+    	element.hide();
+    	
         isCallCallBackFunc = isCallCallBackFunc || false;
         
         if(isCallCallBackFunc) {
@@ -81,3 +98,8 @@ var Assist = function(options) {
     
     this.callbackTest = callback;
 };    
+
+function test() {
+	var a = new Assist({});
+	a.start(300, 300, '생각');
+}
